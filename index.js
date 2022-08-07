4043a8a26afd43436dadd66e15e6c6e6d91c9ad4
@karapetyan.tigran.1998 <karapetyan.tigran.1998@gmail.com>
2022-08-07 17:48:37 +0400

    add responsibility , font

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
    lineWrapper()
     function lineWrapper(){
-        let arrOfLetters = ["Ա" , "Բ" , "Գ" , "Դ" , "Ե" , "Զ" , "Է" , "Ը" , "Թ" , "Ժ" , "Ի" , "Լ" , "Խ" , "Ծ" , "Կ" , "Հ" , "Ձ" , "Ղ" , "Ճ" , "Մ" , "Յ" , "Ն" , "Շ" , "Ո" , "Չ" , "Պ" , "Ջ" , "Ռ" , "Ս" , "Վ" , "Տ" , "Ր" , "Ց" , "ՈՒ" , "Փ" , "Ք" , "և" , "Օ" , "Ֆ"] ;
-        for(let i = 0 ; i < arrOfLetters.length ; i++){
+        let arrOfLetters = ["Ա" , "Բ" , "Գ" , "Դ" , "Ե" , "Զ" , "Է" , "Ը" , "Թ" , "Ժ" , "Ի" , "Լ" , "Խ" , "Ծ" , "Կ" , "Հ" , "Ձ" , "Ղ" , "Ճ" , "Մ" , "Յ" , "Ն" , "Շ" , "Ո" , "Չ" , "Պ" , "Ջ" , "Ռ" , "Ս" , "Վ" , "Տ" , "Ր" , "Ց" , "ՈՒ" , "Փ" , "Ք" , "ԵՎ" , "Օ" , "Ֆ" , "" , "" , "" , "" , "" , "" , "" , "" , "" ] ;
+        for(let i = 0 ; i < arrOfLetters.length; i++){
             let divForLetters = document.createElement('div');
+            if(arrOfLetters[i] !== ""){
             divForLetters.className = ('divWithLetter')
+            } else {
+                divForLetters.className = ('emptyDiv')
+            }
             let text = document.createTextNode(arrOfLetters[i]);
             divForLetters.appendChild(text);
-            document.querySelector(".letters").appendChild(divForLetters);
+            document.querySelector(".divForAllLetters").appendChild(divForLetters);
         }
+
     }
     // document.querySelectorAll('.divWithLetter').forEach(element => element.addEventListener('click' ,function getIndex(){
     //     let indexText = document.createTextNode(arrOfLetters.indexOf(element.innerHTML) + "");
