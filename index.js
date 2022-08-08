e56edefa3b3eb26e3cfe528121e57e6e5bb65ed9
Gevorg <gevorgkarapetyan34@gmail.com>
2022-08-08 16:34:45 +0400

    add responsibility

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
    lineWrapper()
     function lineWrapper(){
-        let arrOfLetters = ["Ա" , "Բ" , "Գ" , "Դ" , "Ե" , "Զ" , "Է" , "Ը" , "Թ" , "Ժ" , "Ի" , "Լ" , "Խ" , "Ծ" , "Կ" , "Հ" , "Ձ" , "Ղ" , "Ճ" , "Մ" , "Յ" , "Ն" , "Շ" , "Ո" , "Չ" , "Պ" , "Ջ" , "Ռ" , "Ս" , "Վ" , "Տ" , "Ր" , "Ց" , "ՈՒ" , "Փ" , "Ք" , "ԵՎ" , "Օ" , "Ֆ" , "" , "" , "" , "" , "" , "" , "" , "" , "" ] ;
+        let arrOfLetters = ["Ա" , "Բ" , "Գ" , "Դ" , "Ե" , "Զ" , "Է" , "Ը" , "Թ" , "Ժ" , "Ի" , "Լ" , "Խ" , "Ծ" , "Կ" , "Հ" , "Ձ" , "Ղ" , "Ճ" , "Մ" , "Յ" , "Ն" , "Շ" , "Ո" , "Չ" , "Պ" , "Ջ" , "Ռ" , "Ս" , "Վ" , "Տ" , "Ր" , "Ց" , "ՈՒ" , "Փ" , "Ք" , "ԵՎ" , "Օ" , "Ֆ"] ;
         for(let i = 0 ; i < arrOfLetters.length; i++){
             let divForLetters = document.createElement('div');
-            if(arrOfLetters[i] !== ""){
-            divForLetters.className = ('divWithLetter')
-            } else {
-                divForLetters.className = ('emptyDiv')
-            }
+            divForLetters.className = ('divWithLetter');
             let text = document.createTextNode(arrOfLetters[i]);
             divForLetters.appendChild(text);
             document.querySelector(".divForAllLetters").appendChild(divForLetters);
         }
-
     }
 
 
