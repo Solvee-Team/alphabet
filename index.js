ed133765b9b8ef8e950a9800b576902b68c5046b
@karapetyan.tigran.1998 <karapetyan.tigran.1998@gmail.com>
2022-08-09 11:37:57 +0400

    in progress...

--- index.js
+++ index.js
@@ -11,3 +11,17 @@
     }
 
 
+//   document.querySelectorAll('.newdiv').forEach(element => element.addEventListener('click' ,function getIndex(){
+//         let indexText = document.createTextNode(arrOfLetters.indexOf(element.innerHTML) + "");
+//         document.querySelector('.divforindexes').appendChild(indexText) ;      
+//         document.querySelector('.divforindexes').style.display = "block" ;  
+//         document.querySelector('.divforindexes').innerHTML = document.querySelector('.divforindexes').innerHTML.charAt(document.querySelector('.divforindexes').innerHTML.length - 1);
+//         let myBtn = document.createElement('button');
+//         myBtn.className = ('btn');
+//         let xForClose = document.createTextNode("x");
+//         myBtn.appendChild(xForClose);
+//         document.querySelector('.divforindexes').appendChild(myBtn) ;   
+//         myBtn.addEventListener('click' , function(){
+//             document.querySelector('.divforindexes').style.display = 'none'; 
+//         })   
+//     }))
