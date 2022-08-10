5c88bab3d119fb4a1cad0a88fab344ed4714351b
@karapetyan.tigran.1998 <karapetyan.tigran.1998@gmail.com>
2022-08-10 22:53:25 +0400

    in progress

--- index.js
+++ index.js
@@ -12,15 +12,22 @@
             document.querySelector(".divForAllLetters").appendChild(divForLetters);
         
     }
+    let arrOfWords = ["արև" , "բալ" , "գնդակ" , "դդում" , "ելակ" , "զատիկ" , "էշ" , "ընկույզ" , "թութակ" , "ժամացույց" , "իլիկ" , "լուսին" , "խաղող" , "ծիածան" , "կացին" , "հաղարջ" , "ձուկ" , "ղեկ" , "ճնճղուկ" , "մոմ" , "յասաման" , "նապաստակ" , "շուն" , "ոզնի" , "չամիչ" , "պայուսակ" , "ջայլամ" , "ռազմիկ" , "սկյուռ" , "վարդ" , "տանձ" , "րոպե" , "ցուլ" , "ուղտ" , "փիղ" , "քաղաք" , "Երևան" , "օձ" , "ֆլամինգո" , ];
+
+
+    let arrOfGrapar = [ ["այբ" , "“Ayb”"] , ["բեն" , "“Ben”"] , ["գիմ" , "“gim”"] , ["դա" , "“da”"] , ["եչ" , "“yech”"] , ["զա" , "“Žhe”"] ,  ["է" , "“Ini”"] , ["ըթ" , "“Ut`”"] , ["թո" , "“T`o”"] , ["ժէ" , "“Žhe”"] , ["ինի" , "“Ini”"] , ["լիւն" , "“Lyun”"] , ["խէ" , "“Xeh”"] , ["ծա" , "“Tza”"] , ["կեն" , "“Ken”"] , ["հո" , "“Ho”"] , ["ձա" , "“dza”"] , ["ղատ" , "“glat”"] , ["ճէ" , "“Cheh”"] , ["մեն" , "“Men”"] , ["յի" , "“Yi”"] , ["նու" , "“Nu”"] , ["շա" , "“Sha”"] , ["ո" , "“vo”"] , ["չա" , "“cha”"] , ["պէ" , "“Peh”"] , ["Ջէ" , "“Jeh”"] , ["ռա" , "“Rra”"] , ["սէ" , "“Seh”"] , ["վէւ" , "“Vew”"] ,  ["տիւն" , "“Tiun”"] , ["րէ" , "“Reh”"] , ["ցո" , "“Co”"] , ["ւիւն" , "“Wyun”"] , ["փիւր" , "“P`iur”"]  ,  ["փիւր" , "“P`iur”"] , ["քէ" , "“Qeh”"] ] ;
+    let objForLetterInfo = Object.fromEntries(arrOfLetters.map((letter) => [letter , ""] ));
+   // console.log(objForLetterInfo)
      document.querySelectorAll('.divWithLetter').forEach(element => element.addEventListener('click' ,function getIndex(){
-        let letter = document.createTextNode(element.innerHTML);
-        document.querySelector('.leftSide').appendChild(letter) ; 
-        document.querySelector('.leftSide').innerHTML = document.querySelector('.leftSide').innerHTML.charAt(document.querySelector('.leftSide').innerHTML.length - 1);     
+        let letterInfo = document.createTextNode(element.innerHTML);
+        let leftSide = document.querySelector('.leftSide');
+        leftSide.innerHTML = "";
+        leftSide.appendChild(letterInfo);
         document.querySelector('.popup').style.opacity = '1'; 
         document.querySelector('.popup').style.visibility = '1'; 
         document.querySelector('.popup').style.zIndex = '1'
     }))
-    document.querySelector('.xForClose').addEventListener('click' , function(){
+    document.querySelector('.xForClose' ).addEventListener('click' , function close(){
         document.querySelector('.popup').style.opacity = '0'; 
         document.querySelector('.popup').style.visibility = '0'; 
         document.querySelector('.popup').style.zIndex = '-1'
