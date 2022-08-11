49d57199a3e497b624e0a415611e71d5e57f510b
Ilonarutyunyan <ilonaarutunan@gmail.com>
2022-08-11 14:19:48 +0400

    pop-up window

--- index.js
+++ index.js
@@ -16,3 +16,7 @@ window.onload = () => {
     })
 }
 
+function PopUp(hideOrshow) {
+    if (hideOrshow == 'hide') document.getElementById('ac-wrapper').style.display = "none";
+    else document.getElementById('ac-wrapper').removeAttribute('style');
+}
